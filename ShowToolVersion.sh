5fe67fc904031442374c2e0c210aba9784dbd231
KaitoMuraoka <kaitoboshi@icloud.com>
2024-03-08 15:23:06 +0900

    add command

--- ShowToolVersion.sh
+++ ShowToolVersion.sh
@@ -2,11 +2,15 @@
 
 typeset -A tools
 tools=(
-  Homebrew "echo 'Homebrew version\n\n' && brew -v"
+  "Homebrew" "brew -v"
+  "CocoaPods" "pod --version"
+  "Homebrew List" "brew list --version"
+  "Docker" "docker --version"
 )
 
-for tool in "${(@k)tools}"; do
-  eval "${tools[$tool]}"
+for tool_name version_commmand in "${(@kv)tools}"; do
+  echo "==============================\n🚀"
+  echo "$tool_name version\n\n" && eval $version_commmand
 done
 
 echo '==============================\n✅ FINISH\n\n'
